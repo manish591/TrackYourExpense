a2b099aba6af3d5faa442ce56e9eefca75363088
manish591 <manishdevrani777@gmail.com>
2021-08-17 15:47:21 +0530

    created list of expenses

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const deleteButton = document.querySelector('.delete');
 
 let valueInView = 0;
 let expenseArray = [];
+const options = {year: 'numeric', month: 'long', day: 'numeric' };
 
 addButton.addEventListener("click", addExpensesToTheList);
 
@@ -18,7 +19,7 @@ function addExpensesToTheList() {
   let amount = Number(inputAmountSpend.value);
   let amountDescription = inputAmountDescription.value;
 
-  if(inputAmountSpend.value === '' || inputAmountDescription.value === '') {
+  if (inputAmountSpend.value === '' || inputAmountDescription.value === '') {
     return console.log('Please input!')
   } else if (Number(inputAmountSpend.value) < 1) {
       return console.log('Please input a valid value!');
@@ -26,21 +27,29 @@ function addExpensesToTheList() {
 
   expenseObj.amount = amount;
   expenseObj.description = amountDescription;
+  expenseObj.moment = new Date();
 
   expenseArray.push(expenseObj);
-  createExpenseLists(amount, amountDescription);
+
+  let arrayListItems = expenseArray.map(item => createExpenseLists(item.amount, item.description, item.moment))
+  let joinedList = arrayListItems.join("");
+
+
   updateTotalValue(amount, totalExpense);
+  expenseListInView.innerHTML = joinedList;
 }
 
-function createExpenseLists(rupees, name) {
-    let list = document.createElement("div");
-    list.classList.add("list");
-    expenseListInView.appendChild(list);
-    list.innerHTML = `
-     <p class="name">${name}</p>
-      <div class="other">
-        <strong>${rupees}</strong>
-        <button class="delete">x</button>
+function createExpenseLists(rupees, name, moment) {
+    return `
+      <div class="list"> 
+        <div class="listtitle">
+          <p class="name">${name}</p>
+          <span class="date">${moment.toLocaleDateString('en-US', options)}</span>
+        </div>
+        <div class="other">
+          <strong>${rupees}</strong>
+          <button class="delete">x</button>
+        </div>
       </div>`;
 }
 
